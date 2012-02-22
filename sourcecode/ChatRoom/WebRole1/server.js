39121dc232ce5523d162e486101d86b433c8abc2
unknown <AIDAN@AIDAN-PC.(none)>
2012-02-22 19:46:43 +1100

    Signed-off-by: unknown <AIDAN@AIDAN-PC.(none)>

--- sourcecode/ChatRoom/WebRole1/server.js
+++ sourcecode/ChatRoom/WebRole1/server.js
@@ -1,89 +1,57 @@
-var app = require('http').createServer(handler)  
-, io = require('socket.io').listen(app)  
-, fs = require('fs');
+var app = require('http').createServer(handler);
+  
+var io = require('socket.io').listen(app);  
+var fs = require('fs');
 
 var static = require('node-static');
 var file = new(static.Server)('./public');
 
-
-
 app.listen(process.env.port || 4567);
 
 function handler (req, res) {
-
-
-
     req.addListener('end', function () {
-    //
+
     // Serve static files!
-    //
     file.serve(req, res);
   });
+}
 
-//sxtart
-  // fs.readFile(__dirname + '/index.html',
-  //   function (err, data) { 
-  //      if (err) {      res.writeHead(500);
-  //            return res.end('Error loading index.html');
-  //                } 
-                 
-  //                   res.writeHead(200); 
-  //                      res.end(data); 
-  //                       });
-                
-  //               //end
-            }
+var nicknames = new Array();
+      
+io.sockets.on('connection', function (socket) {
+      console.log('in connect function socket...' + socket);
+       socket.emit('update', { data: 'you are now connected' });
 
 
-                
+      socket.on('user message', function (data) {
+                var currentDate = new Date();
 
-                io.sockets.on('connection', function (socket) {
-                    console.log('in connect function socket...' + socket);
-                    socket.emit('update', { data: 'you are now connected' });
+                 console.log(data);
 
-                   socket.on('user message', function (data) {
+                 //back to self
+                 socket.emit('update', { message: data, nick: 'socket.nickname', date : currentDate });
+                 //send to everyone
+                 socket.broadcast.emit('update', { message: data, nick: 'socket.nickname' , date : currentDate});
+                 }
+               );
 
-                      var currentDate = new Date();
+      socket.on('my other event', function (data) {
+                  console.log(data);
+                  }
+                );
 
+      socket.on('nickname', function (nick) {
+                nicknames.push({name: nick});
+                nicknames[nick] = socket.nickname = nick;
+                socket.broadcast.emit('announcement', { message: nick + ' has joined', nick: socket.nickname });
+                //hack broadcast sshould do this...
+                socket.emit('announcement', { message: nick + ' has joined', nick: socket.nickname });
+                io.sockets.emit('nicknames', nicknames);
+                    }
+                );
 
-                        console.log('what it looks like');
-                        console.log(data);
-
-                        //back to self
-                        socket.emit('update', { message: data, nick: 'socket.nickname', date : currentDate });
-
-                        //send to everyone
-                        socket.broadcast.emit('update', { message: data, nick: 'socket.nickname' , date : currentDate});
-                    });
-
-                    socket.on('my other event', function (data) {
-                        console.log(data);
-                    });
-
-
-                    // this bit to do with nick names....
-                    var nicknames = new Array();
-
-                    socket.on('nickname', function (nick) {
-
-
-                    nicknames.push({name: nick});
-
-
-                        nicknames[nick] = socket.nickname = nick;
-                        socket.broadcast.emit('announcement', { message: nick + ' has joined', nick: socket.nickname });
-
-                        //hack broadcast sshould do this...
-                        socket.emit('announcement', { message: nick + ' has joined', nick: socket.nickname });
-
-                        io.sockets.emit('nicknames', nicknames);
-
-                    });
-
-
-
-
-                });
+                }
+              );
 
 
 
