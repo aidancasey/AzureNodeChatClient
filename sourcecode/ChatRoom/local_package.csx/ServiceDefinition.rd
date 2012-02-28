adbd0b1745caac8b04c9f98d766e55652ab7c30b
unknown <AIDAN@AIDAN-PC.(none)>
2012-02-28 20:59:19 +1100

    Signed-off-by: unknown <AIDAN@AIDAN-PC.(none)>

--- sourcecode/ChatRoom/local_package.csx/ServiceDefinition.rd
+++ sourcecode/ChatRoom/local_package.csx/ServiceDefinition.rd
@@ -1,32 +1,32 @@
 ﻿<?xml version="1.0" encoding="utf-8"?>
-<serviceModel xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" name="AidansChattyRoom1" generation="1" functional="0" release="0" Id="2e762c95-f3bb-48a3-a49e-e89766f4887a" dslVersion="1.2.0.0" xmlns="http://schemas.microsoft.com/dsltools/RDSM">
+<serviceModel xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" name="AidansChattyRoom3" generation="1" functional="0" release="0" Id="27eb6ad6-e658-4457-83d1-b5d590891eb6" dslVersion="1.2.0.0" xmlns="http://schemas.microsoft.com/dsltools/RDSM">
   <groups>
-    <group name="AidansChattyRoom1Group" generation="1" functional="0" release="0">
+    <group name="AidansChattyRoom3Group" generation="1" functional="0" release="0">
       <componentports>
         <inPort name="WebRole1:Endpoint1" protocol="http">
           <inToChannel>
-            <lBChannelMoniker name="/AidansChattyRoom1/AidansChattyRoom1Group/LB:WebRole1:Endpoint1" />
+            <lBChannelMoniker name="/AidansChattyRoom3/AidansChattyRoom3Group/LB:WebRole1:Endpoint1" />
           </inToChannel>
         </inPort>
       </componentports>
       <settings>
         <aCS name="WebRole1Instances" defaultValue="[1,1,1]">
           <maps>
-            <mapMoniker name="/AidansChattyRoom1/AidansChattyRoom1Group/MapWebRole1Instances" />
+            <mapMoniker name="/AidansChattyRoom3/AidansChattyRoom3Group/MapWebRole1Instances" />
           </maps>
         </aCS>
       </settings>
       <channels>
         <lBChannel name="LB:WebRole1:Endpoint1">
           <toPorts>
-            <inPortMoniker name="/AidansChattyRoom1/AidansChattyRoom1Group/WebRole1/Endpoint1" />
+            <inPortMoniker name="/AidansChattyRoom3/AidansChattyRoom3Group/WebRole1/Endpoint1" />
           </toPorts>
         </lBChannel>
       </channels>
       <maps>
         <map name="MapWebRole1Instances" kind="Identity">
           <setting>
-            <sCSPolicyIDMoniker name="/AidansChattyRoom1/AidansChattyRoom1Group/WebRole1Instances" />
+            <sCSPolicyIDMoniker name="/AidansChattyRoom3/AidansChattyRoom3Group/WebRole1Instances" />
           </setting>
         </map>
       </maps>
@@ -45,8 +45,8 @@
             </resourcereferences>
           </role>
           <sCSPolicy>
-            <sCSPolicyIDMoniker name="/AidansChattyRoom1/AidansChattyRoom1Group/WebRole1Instances" />
-            <sCSPolicyFaultDomainMoniker name="/AidansChattyRoom1/AidansChattyRoom1Group/WebRole1FaultDomains" />
+            <sCSPolicyIDMoniker name="/AidansChattyRoom3/AidansChattyRoom3Group/WebRole1Instances" />
+            <sCSPolicyFaultDomainMoniker name="/AidansChattyRoom3/AidansChattyRoom3Group/WebRole1FaultDomains" />
           </sCSPolicy>
         </groupHascomponents>
       </components>
@@ -57,11 +57,11 @@
     </group>
   </groups>
   <implements>
-    <implementation Id="9988c441-c1cc-45cd-982b-6391ffc68c6a" ref="Microsoft.RedDog.Contract\ServiceContract\AidansChattyRoom1Contract@ServiceDefinition">
+    <implementation Id="c1b62588-85e6-4360-9593-f22af0a6ef9c" ref="Microsoft.RedDog.Contract\ServiceContract\AidansChattyRoom3Contract@ServiceDefinition">
       <interfacereferences>
-        <interfaceReference Id="c2d56ff9-d7c2-499d-b576-e5d9b1729473" ref="Microsoft.RedDog.Contract\Interface\WebRole1:Endpoint1@ServiceDefinition">
+        <interfaceReference Id="5dc19c93-48fc-4a7a-97cd-302220f08eac" ref="Microsoft.RedDog.Contract\Interface\WebRole1:Endpoint1@ServiceDefinition">
           <inPort>
-            <inPortMoniker name="/AidansChattyRoom1/AidansChattyRoom1Group/WebRole1:Endpoint1" />
+            <inPortMoniker name="/AidansChattyRoom3/AidansChattyRoom3Group/WebRole1:Endpoint1" />
           </inPort>
         </interfaceReference>
       </interfacereferences>
--- sourcecode/ChatRoom/local_package.csx/roles/WebRole1/approot/server.js
+++ sourcecode/ChatRoom/local_package.csx/roles/WebRole1/approot/server.js
@@ -1,85 +1,87 @@
-var app = require('http').createServer(handler)  
-, io = require('socket.io').listen(app)  
-, fs = require('fs');
+var app = require('http').createServer(handler);
+  
+var io = require('socket.io').listen(app);  
+var fs = require('fs');
 
 var static = require('node-static');
 var file = new(static.Server)('./public');
 
-
-
 app.listen(process.env.port || 4567);
 
 function handler (req, res) {
-
-
-
     req.addListener('end', function () {
-    //
+
     // Serve static files!
-    //
     file.serve(req, res);
   });
+}
 
-//sxtart
-  // fs.readFile(__dirname + '/index.html',
-  //   function (err, data) { 
-  //      if (err) {      res.writeHead(500);
-  //            return res.end('Error loading index.html');
-  //                } 
-                 
-  //                   res.writeHead(200); 
-  //                      res.end(data); 
-  //                       });
-                
-  //               //end
-            }
+var nicknames = new Array();
+      
+io.sockets.on('connection', function (socket) {
+      console.log('in connect function socket...' + socket);
+       socket.emit('update', { data: 'you are now connected' });
 
 
-                
-
-                io.sockets.on('connection', function (socket) {
-                    console.log('in connect function socket...' + socket);
-                    socket.emit('update', { data: 'you are now connected' });
-
-                   socket.on('user message', function (data) {
-
-                      var currentDate = new Date();
-
-
-                        console.log('what it looks like');
-                        console.log(data);
-
-                        //back to self
-                        socket.emit('update', { message: data, nick: 'socket.nickname', date : currentDate });
-
-                        //send to everyone
-                        socket.broadcast.emit('update', { message: data, nick: 'socket.nickname' , date : currentDate});
-                    });
-
-                    socket.on('my other event', function (data) {
-                        console.log(data);
-                    });
-
-
-                    // this bit to do with nick names....
-                    var nicknames = {};
-                    socket.on('nickname', function (nick) {
-
-                        nicknames[nick] = socket.nickname = nick;
-                        socket.broadcast.emit('announcement', { message: nick + ' has joined', nick: socket.nickname });
-
-                        //hack broadcast sshould do this...
-                        socket.emit('announcement', { message: nick + ' has joined', nick: socket.nickname });
-
-                        io.sockets.emit('nicknames', nicknames);
-
-                    });
+      socket.on('user message', function (data) {
 
 
 
+                 console.log(data);
 
-                });
+                 //back to self
+                 socket.emit('update', { message: data, nick: 'socket.nickname', date : CurrentDateAndTime() });
+                 //send to everyone
+                 socket.broadcast.emit('update', { message: data, nick: 'socket.nickname' , date : CurrentDateAndTime()});
+                 }
+               );
 
+      socket.on('my other event', function (data) {
+                  console.log(data);
+                  }
+                );
 
+      socket.on('nickname', function (nick) {
+                nicknames.push({name: nick});
+                nicknames[nick] = socket.nickname = nick;
+                socket.broadcast.emit('announcement', { message: nick + ' has joined!',  date : CurrentDateAndTime() });
+                //hack broadcast sshould do this...
+                socket.emit('announcement', { message: nick + ' has joined !', date : CurrentDateAndTime() });
+                io.sockets.emit('nicknames', nicknames);
+                    }
+                );
+
+                }
+              );
+
+function CurrentDateAndTime()
+{
+var currentTime = new Date();
+var month = currentTime.getMonth() + 1;
+if (month < 10){
+  month = "0" + month;
+}
+
+var day = currentTime.getDate()
+if (day < 10){
+  day = "0" + day;
+}
+
+var year = currentTime.getFullYear()
+
+var hours = currentTime.getHours();
+
+var minutes = currentTime.getMinutes();
+if (minutes < 10){
+  minutes = "0" + minutes;
+}
+var seconds = currentTime.getSeconds();
+if (seconds < 10){
+  seconds = "0" + seconds;
+}
+
+  var now =  day + '/' + month + '/' + year + '   :' +  hours + ':' + minutes + ':' + seconds;
+return now;  
+}
 
 
